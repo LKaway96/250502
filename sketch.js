994c5589a92de2f7389665c95e28f3d7d13c4021
LKaway96 <dreamataiwan@gmail.com>
2025-05-02 10:34:27 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let capture;
+let overlayGraphics;
 
 function setup() {
   // 設定畫布大小為視窗大小
@@ -11,6 +12,13 @@ function setup() {
   capture = createCapture(VIDEO);
   capture.size(windowWidth * 0.8, windowHeight * 0.8);
   capture.hide(); // 隱藏原始的攝影機畫面
+
+  // 建立與攝影機畫面相同大小的圖形緩衝區
+  overlayGraphics = createGraphics(capture.width, capture.height);
+  overlayGraphics.fill(255, 0, 0, 150); // 半透明紅色
+  overlayGraphics.textSize(32);
+  overlayGraphics.textAlign(CENTER, CENTER);
+  overlayGraphics.text("Overlay Text", overlayGraphics.width / 2, overlayGraphics.height / 2);
 }
 
 function draw() {
@@ -34,8 +42,11 @@ function draw() {
   push();
   translate(width, 0); // 將畫布原點移到右上角
   scale(-1, 1); // 水平翻轉畫布
-  image(capture, x, y, displayWidth, displayHeight);
+  image(capture, -x - displayWidth, y, displayWidth, displayHeight);
   pop();
+
+  // 繪製 overlayGraphics 到畫布上，顯示在攝影機畫面上方
+  image(overlayGraphics, x, y, displayWidth, displayHeight);
 }
 
 function windowResized() {

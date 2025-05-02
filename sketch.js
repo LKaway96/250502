3532f937914690182fe87bc216e9cc14a1f0d597
LKaway96 <dreamataiwan@gmail.com>
2025-05-02 11:03:39 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let capture;
-let createGraphics
+let graphics;
+
 function setup() {
   // 設定畫布大小為視窗大小
   createCanvas(windowWidth, windowHeight);
@@ -11,6 +12,14 @@ function setup() {
   capture = createCapture(VIDEO);
   capture.size(windowWidth * 0.8, windowHeight * 0.8);
   capture.hide(); // 隱藏原始的攝影機畫面
+
+  // 使用 createGraphics 產生與攝影機畫面相同大小的圖形緩衝區
+  graphics = createGraphics(capture.width, capture.height);
+  graphics.background(255, 0, 0); // 設定緩衝區背景為紅色（可自行修改）
+  graphics.fill(255);
+  graphics.textSize(32);
+  graphics.textAlign(CENTER, CENTER);
+  graphics.text('Overlay Text', graphics.width / 2, graphics.height / 2); // 在緩衝區中繪製文字
 }
 
 function draw() {
@@ -36,6 +45,9 @@ function draw() {
   scale(-1, 1); // 水平翻轉畫布
   image(capture, x, y, displayWidth, displayHeight);
   pop();
+
+  // 在攝影機畫面上方繪製 createGraphics 的內容
+  image(graphics, x, y, displayWidth, displayHeight);
 }
 
 function windowResized() {
